b888baed9a66b7de6af1296dc32e459b68cd7e73
eadogan <e.ahmetdogan@gmail.com>
2021-04-17 18:12:25 +0100

    Added Start script/Refactor unnecessary code

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -3,16 +3,17 @@ const request = require('request')
 const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+address+'.json?access_token=pk.eyJ1IjoiYXZmOTM2MTEiLCJhIjoiY2tuaGl0Z2k2Mnp3bTJxdGExand1d28xdCJ9.Kwo_E0OOh1e5AR5M3tUq_A'
 
-    request({ url, json: true }, (error, { body }) => {
+    request({ url, json: true }, (error, { body }) => { 
         if(error){
             callback('Unable to connect to location services!', undefined)
-        } else if(body.features.length === 0) {
+        } else if(body.features === undefined) {
             callback('Unable to find location. Try another search.', undefined)
         } else {
             callback(undefined, {
+                location: body.features[0].place_name,
                 longitude: body.features[0].center[0],
-                latitude: body.features[0].center[1],
-                location: body.features[0].place_name
+                latitude: body.features[0].center[1]
+                
             })
         }
     })
